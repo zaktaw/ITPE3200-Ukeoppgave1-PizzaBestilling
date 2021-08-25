7f128658b705bb44926cf25a020c0beb975201ef
zaktaw <zaktaw@gmail.com>
2021-08-25 09:12:35 +0200

    Add packages EntityFrameworkCore and Sqlite. Add model for database.

---
+++ Models/KundeBestillingDb.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITPE3200_Ukeoppgave1_PizzaBestilling.Models
+{
+    public class KundeBestillingDb :DbContext
+    {
+        public KundeBestillingDb(DbContextOptions<KundeBestillingDb> options) : base(options)
+        {
+            Database.EnsureCreated(); // opprette databasen når vi starter applikasjonen
+        }
+
+        public DbSet<Kunde> Kunder { get; set; }
+        public DbSet<Bestilling> Bestillinger{ get; set; }
+    }
+}
